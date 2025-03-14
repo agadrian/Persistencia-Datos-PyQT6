d039bbe5733c35c2526989458d65ff4a3e1e41ac
agadrian <arroyogomezadrian@gmail.com>
2025-03-15 00:28:06 +0100

    .

--- db_functions.py
+++ db_functions.py
@@ -1,21 +1,17 @@
 import sqlite3
 import pyrebase
 import os
-from dotenv import load_dotenv
-
-# Cargar el .env
-load_dotenv()
 
 # Configuracion Firebase
 firebase_config = {
-    "apiKey": os.getenv("FIREBASE_API_KEY"),
-    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
-    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
-    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
-    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
-    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
-    "appId": os.getenv("FIREBASE_APP_ID"),
-    "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID")
+    "apiKey": "AIzaSyCQy1F8kgeu14Ue9hIZvmt2BFrsbKuNT8Y",
+    "authDomain": "proyecto-pyqt.firebaseapp.com",
+    "databaseURL": "",
+    "projectId": "proyecto-pyqt",
+    "storageBucket": "proyecto-pyqt.firebasestorage.app",
+    "messagingSenderId": "456522722623",
+    "appId": "1:456522722623:web:1c718668b012138f29778a",
+    "measurementId": "G-TXQWM1E404"
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
